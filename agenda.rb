7059514bced3cd87d1fb6f42fd418afb166b1270
Thiago <thiagooffice2@gmail.com>
2023-08-04 19:56:39 -0300

    feat: method show and  method update

--- agenda.rb
+++ agenda.rb
@@ -23,6 +23,39 @@ def adicionar_contato
     @agenda << {nome: nome, telefone: telefone}
 end
 
+def ver_contato
+    print "Qual nome você deseja: "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        if contato[:nome].downcase.include?(nome.downcase)
+            puts "#{contato[:nome]} - #{contato[:telefone]}"
+        end
+    end
+    puts "---------------------------------"
+end
+
+def editar_contato
+    print "Qual nome deseja editar: "
+    nome = gets.chomp
+
+    @agenda.each do |contato|
+        if contato[:nome].downcase == (nome.downcase)
+            print "Nome para editar (Se quiser manter o mesmo nome , aperte enter.)"
+            nome_salvo = contato[:nome]
+
+            contato[:nome] = gets.chomp
+            contato[:nome] = contato[:nome].empty? ? nome_salvo : contato[:nome]
+
+            print "Nome para editar (Se quiser manter o mesmo telefone , aperte enter.)"
+            telefone_salvo = contato[:telefone]
+
+            contato[:telefone] = gets.chomp
+            contato[:telefone] = contato[:telefone].empty? ? telefone_salvo : contato[:telefone]
+        end
+    end
+end
+
 loop do
 puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
     codigo = gets.chomp.to_i
@@ -35,5 +68,9 @@ puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. R
         todos_contatos
     when codigo == 2
         adicionar_contato
+    when codigo == 3
+        ver_contato
+    when codigo == 4
+        editar_contato
     end
 end
